0da7b5b307ccc18179119f2246a045f0e0d123ba
Jarl Gullberg <Jarl.Gullberg@gmail.com>
2019-12-30 19:23:44 +0100

    Not reproducing

--- EFCore.Repro/ReproEntity.cs
+++ EFCore.Repro/ReproEntity.cs
@@ -1,12 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace EFCore.Repro
 {
     public class ReproEntity
     {
         public int ID { get; set; }
 
-        [Required]
         public string Data { get; set; }
     }
 }
